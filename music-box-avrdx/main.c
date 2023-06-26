74a3567d915b5e3205959ba6efe0cdac71a9e5b8
aaa <bbb>
2023-06-26 15:31:46 +0800

    Add files.

---
+++ music-box-avrdx/main.c
@@ -0,0 +1,13 @@
+#include <atmel_start.h>
+#include <stdio.h>
+
+int main(void)
+{
+	/* Initializes MCU, drivers and middleware */
+	atmel_start_init();
+
+	printf("Initialized.\n");
+	/* Replace with your application code */
+	while (1) {
+	}
+}
