05c8ae48b7a7177a8929eeee13b07353c459bd5c
yuan <ayrw@163.com>
2023-07-09 23:15:01 +0800

    Disable rounding.

--- music-box-avrdx/WavetableSynthesizer/WaveTable.h
+++ music-box-avrdx/WavetableSynthesizer/WaveTable.h
@@ -1,15 +1,16 @@
 #ifndef __WAVETABLE__
 #define __WAVETABLE__
-// Sample's base frequency: 523.629906 Hz
+// Sample name: True Vibe C5
+// Sample's base frequency: 522.9098067244921 Hz
 // Sample's sample rate: 32000 Hz
-
-#define WAVETABLE_LEN 2608
-#define WAVETABLE_ATTACK_LEN 1998
-#define WAVETABLE_LOOP_LEN 610
+#define WAVETABLE_LEN 55318
+#define WAVETABLE_ATTACK_LEN 53789
+#define WAVETABLE_LOOP_LEN 1529
+#define WAVETABLE_ACTUAL_LEN 55319
 
 #ifndef __ASSEMBLER__
 #include <stdint.h>
-extern const int8_t __attribute__((section (".far_flash"))) WaveTable[WAVETABLE_LEN];
+extern const int8_t WaveTable[WAVETABLE_ACTUAL_LEN];
 extern const uint16_t WaveTable_Increment[];
 #else
 .extern	WaveTable
